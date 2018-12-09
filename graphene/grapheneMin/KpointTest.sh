f091dda3daec9f30ec10ac9695eda2c035b87c7d
orivenlikon <40763569+orivenlikon@users.noreply.github.com>
2018-12-09 19:28:24 +0800

    kpointTest

---
+++ graphene/grapheneMin/KpointTest.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [[ ! -f "WAVECAR" ]];then
+touch cache
+else
+mv WAVECAR cache
+fi
+for i in 2 4 6 8 10
+do
+cat > KPOINTS <<!
+Automatic generation
+0
+Gama Centered
+$i 	$i 	$i
+0.0 0.0 0.0
+!
+echo "k mesh = $i x $i x $i"
+qsub vasp.5.4.1.pbs
+E=$(grep "TOTEN" OUTCAR | tail -n 1)
+KP=$(grep "irreducible" OUTCAR | tail -n 1)
+T=$(grep "Elapsed time" OUTCAR | tail -n 1)
+echo $i $KP $E $T >> KpointTest
+done
